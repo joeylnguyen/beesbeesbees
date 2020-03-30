efe8ae432840d45ffce6f5cec0c94603715f2e54
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 10:03:38 -0700

    Create Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,14 @@
 var Grub = function() {
+  // Add age property with value 0
+  this.age = 0;
+  // Add color property with value pink
+  this.color = 'pink';
+  // Add food property with value jelly
+  this.food = 'jelly';
+  //
 };
 
+// Add eat method to Grub prototype
+Grub.prototype.eat = function() {
+
+};
