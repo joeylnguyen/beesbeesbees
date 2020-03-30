b6c61321a9470613ac6fee462144efaaba362fd0
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 10:45:31 -0700

    Create ForagerBee subclass

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,24 @@
 var ForagerBee = function() {
+  // Call Bee
+  Bee.call(this);
+  // Add age property with value of 10
+  this.age = 10;
+  // Add job property with value of find pollen
+  this.job = 'find pollen';
+  // Add canFly property with value of true
+  this.canFly = true;
+  // Add treasureChest property that is set to []
+  this.treasureChest = [];
+
 };
 
+
+// Set prototype to Bee prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+// Set constructor function to itself
+ForagerBee.prototype.constructor = ForagerBee;
+// Add forage method that adds treasure to the treasureChest
+ForagerBee.prototype.forage = function(treasure) {
+  // Push treasure to treasureChest
+  this.treasureChest.push(treasure);
+ };
