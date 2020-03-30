abd30d96f2410b3655abcb7e302d4359d24d038e
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 10:39:29 -0700

    Create HoneyMakerBee subclass

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
 var HoneyMakerBee = function() {
+  // Call Bee superclass
+  Bee.call(this);
+  // Add age property with value 10
+  this.age = 10;
+  // Add job property with value make honey
+  this.job = 'make honey';
+  // Add a honeyPot property that is set to 0
+  this.honeyPot = 0;
 };
 
+// Set the HoneyMakerBee prototype to the Bee prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// Set the HoneyMakerbee constructor to itself
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+// Add makeHoney method that adds to to honeyBee's honeyPot to prototype
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+// Add giveHoney method that subtracts 1 from honeyBee's honeyPot to prototype
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
