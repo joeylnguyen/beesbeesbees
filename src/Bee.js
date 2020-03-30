90d952307024626237a07ff34ba621bf6b9e844f
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-30 10:20:04 -0700

    Create Bee subclass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  // Call the Grub superclass
+  Grub.call(this);
+  // Add age property with value 5
+  this.age = 5;
+  // Add color property with value yellow
+  this.color = 'yellow';
+  // Add job property set to an empty object
+  this.job = 'keep on growing';
+
 };
 
+// Set Bee prototype to Grub prototype
+Bee.prototype = Object.create(Grub.prototype);
+// Set constructor to Bee
+Bee.prototype.constructor = Bee;
